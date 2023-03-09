232db7dd95b6093f15f8cbb445c740fb8ec6f938
Aqua908 <nname6034@gmail.com>
2023-03-09 22:45:06 +0400

    build error fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import React, {useState} from 'react';
-
+import React from 'react';
 import './index.css'
-
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Header from './Components/Header';
 
 
 function App() {
   
-  function refreshPage(){ 
-    window.location.reload(); 
-}
+
 
 return (
  
@@ -17,21 +14,7 @@ return (
     <div className='App'>
       <Header/>
       <div className='MainBackground'></div>
-          <div className='HeaderContainer'>
-    <div>
-    <Link to="/" className='LogoLink'> <img  className="LogoStyles" alt='Logo' src={Logo}/></Link>
 
-</div>
-<div>
-<a className='HeaderLinkStyle' href='https://www.dubbledex.com' target="_blank"><img className="DocsIconStyle"  src={DocsIcon} alt='DocsIcon'/><span className='SpanLink '>Docs</span></a>
-</div>
-<div className='ButtonAndLinkDiv'>
-<Link to="/Tokenomics" className='HeaderLinkTokenomicStyle'><img className="TokenomicIconStyle" src={TokenomicIcon} alt='TokenomicIcon'/><span className='SpanLink'>Tokenomic</span></Link>
-</div>
-<div className='ButtonAndLinkDiv'>
-<Link role="link" aria-disabled="true" className='HeaderLinkLaunchPadStyle DisabledLink'><img className="LaunchpadIconStyle DisabledIcon" src={LaunchPadIcon} alt='LaunchpadIcon'/><span className='SpanLink DisabledSpan'>Coming Soon</span></Link>
-</div>
-</div>
 
 
 
